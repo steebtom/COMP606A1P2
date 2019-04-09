f8ee27ec1d6b439ac3d79458b050751e09c6b0a7
steeb-wintec <steeb.webdev@gmail.com>
2019-04-10 00:24:58 +1200

    index & header

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
 <?php
-echo "hi";
- ?>
+require "header.php";
+?>
+<head>
+<link rel="stylesheet" type="text/css" media="screen" href="./main.css">
+</head>
+<main>
+    <div class=wrapper>
+    
+    <?php
+    if(!isset($_SESSION['userid']))
+    {
+        echo '<div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Thank You for visiting us.</h1>
+          <p class="lead">Log in to continue !</p>
+        </div>
+      </div>';
+    }
+    else
+    {
+        echo '<div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Welcome back.</h1>
+          <p class="lead">You are now logged in.</p>
+        </div>
+        </div>';
+    }
+    ?>
+    </div>
+</main>
+
+<?php
+require "footer.php"
+?>
